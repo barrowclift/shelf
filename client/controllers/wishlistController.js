8f16db9f0e327bb2931955e2e5f67f523f6f6918
Marc Barrowclift <marc@barrowclift.me>
2018-01-25 20:45:46 +0000

    Apparently domain wasn't a required socket.io field anyway

--- client/controllers/wishlistController.js
+++ client/controllers/wishlistController.js
@@ -37,7 +37,7 @@ angular
         }
     }
 
-    var socket = io.connect(config.PUBLIC_WEB_ADDRESS);
+    var socket = io.connect();
     socket.on("connect", function() {
         logger.logInfo(CLASS_NAME, "Client has connected to the server");
     });
--- client/shared/config.js
+++
@@ -1,8 +0,0 @@
-(function(exports) {
-
-	/**
-	 * Required to be known for allowing back-and-forth socket communication
-	 */
-	exports.PUBLIC_WEB_ADDRESS = "https://YOUR_ADDRESS";
-
-})(typeof exports === 'undefined' ? this['config'] = {} : exports);
